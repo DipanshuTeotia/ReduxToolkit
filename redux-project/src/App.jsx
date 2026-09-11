7322e3517e3ed02c73d612a7f1512d2e0a87f877
dipanshu teotia <dipanshuteotia007@gmail.com>
2026-09-12 03:28:44 +0530

    fixed a typo

--- redux-project/src/App.jsx
+++ redux-project/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { fetchPhotos, fetchVideo } from './api/mediaapi'
+import { fetchPhotos, fetchVideo } from './api/mediaApi'
 import SearchBar from './components/SearchBar'
 import Tabs from './components/Tabs'
+import ResultGrid from './components/ResultGrid'
 
 const App = () => {
   return (
     <div className='h-screen w-full bg-black text-white'>
       <SearchBar/>
       <Tabs/>
+      <ResultGrid/>
     </div>
   )
 }
---
+++ redux-project/src/components/ResultGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResultGrid = () => {
+  return (
+    <div>
+      loading
+    </div>
+  )
+}
+
+export default ResultGrid
