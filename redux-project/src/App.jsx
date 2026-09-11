826da39da4dbada00c5e658b16dd60ba51426d55
dipanshu teotia <dipanshuteotia007@gmail.com>
2026-09-11 15:03:05 +0530

    file structure and project setup done

---
+++ redux-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-black text-white'>
+      i am dipanshu teotia 
+    </div>
+  )
+}
+
+export default App
