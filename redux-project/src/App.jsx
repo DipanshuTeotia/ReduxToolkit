243a93f281eab590b99ce45936c49a68ad4f61e7
dipanshu teotia <dipanshuteotia007@gmail.com>
2026-09-11 23:37:23 +0530

    api fetch done

--- redux-project/src/App.jsx
+++ redux-project/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { fetchPhotos, fetchVideo } from './api/mediaapi'
 
 const App = () => {
   return (
     <div className='h-screen w-full bg-black text-white'>
-      i am dipanshu teotia 
+      <button
+      onClick={async ()=>{
+        const data = await fetchPhotos('dog');
+        console.log(data.data.results);
+      }}
+      className='m-4 p-1 bg-gray-500 rounded'>get photos</button>
+
+      <button
+      onClick={async()=>{
+        const data =await fetchVideo('cat');
+        console.log(data.data.videos);
+      }}
+      className='m-4 p-1 bg-gray-500 rounded'>get videos</button>
     </div>
   )
 }
---
+++ redux-project/src/api/mediaApi.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY;
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY;
+
+export async function fetchPhotos(query,page=1,per_page=20){
+    // get the data from unsplash using axios 
+    const res = await axios.get('https://api.unsplash.com/search/photos',{
+        // this query will tell what u asked for like cat or dog
+        // page will tell which page 
+        // per_page will limit the items u want from a page 
+        params:{query,page,per_page},
+        // header will tell us authorization -> client-id YOU_ACCESS_KEY
+        headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
+    })
+    return res;
+}
+
+export async function fetchVideo(query,per_page=15){
+    const res =await axios.get('https://api.pexels.com/v1/videos/search',{
+        params:{query,per_page},
+        headers:{PEXELS_KEY}
+    })
+    return res;
+}
