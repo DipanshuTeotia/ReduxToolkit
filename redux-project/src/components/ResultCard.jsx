2f0abb613a1decd304a9f74540e3a36ce00d1dcd
dipanshu teotia <dipanshuteotia007@gmail.com>
2026-09-13 01:20:53 +0530

    added the data in local storage

--- redux-project/src/components/ResultCard.jsx
+++ redux-project/src/components/ResultCard.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const ResultCard = ({item}) => {
+  const addToCollection = (item)=>{
+    const oldData = JSON.parse(localStorage.getItem('collection')) || [];
+    const newData = [...oldData,item];
+    localStorage.setItem("collection",JSON.stringify(newData));
+    console.log(JSON.parse(localStorage.getItem("collection")));
+  }
   return (
     <div className='h-80 w-[20vw] bg-white rounded-xl relative overflow-hidden'>
 
@@ -11,7 +17,11 @@ const ResultCard = ({item}) => {
 
       <div className='absolute bottom-0 flex justify-between items-center px-2 py-2 w-full text-white font-bold text-center overflow-hidden'>
         <h2 className='capitalize'>{item.title}</h2>
-        <button className='bg-blue-500 px-3 py-1 rounded active:scale-90 justify-center items-center cursor-pointer'>Save</button>
+        <button
+        onClick={()=>{
+          addToCollection(item);
+        }}
+        className='bg-blue-500 px-3 py-1 rounded active:scale-90 justify-center items-center cursor-pointer'>Save</button>
       </div>
 
     </div>
