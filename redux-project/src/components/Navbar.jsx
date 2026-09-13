190467646c862f1ffa72f6b6280f8a716a5407ca
dipanshu teotia <dipanshuteotia007@gmail.com>
2026-09-13 16:53:11 +0530

    added link to meadia search

--- redux-project/src/components/Navbar.jsx
+++ redux-project/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
       <div className='flex justify-between items-center p-4 bg-gray-700'>
-          <h1 className='text-2xl'>Media Search</h1>
+          <Link to='/' className='text-2xl'>Media Search</Link>
           <div className='flex gap-2 text-black'>
               <Link to='/' className='bg-white px-1 rounded'>Search</Link>
               <Link to='/collection' className='bg-white px-1 rounded'>Collection</Link>
