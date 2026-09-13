070f0fefbf864023f9808a29e872eaec48c1fef5
dipanshu teotia <dipanshuteotia007@gmail.com>
2026-09-13 16:45:17 +0530

    added the remove feature in collection page

--- redux-project/src/pages/CollectionPage.jsx
+++ redux-project/src/pages/CollectionPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import { useSelector } from "react-redux";
+import CollectionCard from '../components/CollectionCard';
 const CollectionPage = () => {
+  const collection = useSelector((state)=>state.collection.items);
   return (
-    <div>
-      collectionPages
+    <div className='flex justify-start gap-3 flex-wrap overflow-auto h-[80%] w-full p-2'>
+      {collection.map((item,idx)=>{
+        return <div
+         key={idx}> <CollectionCard item={item}/> </div>
+      })}
     </div>
   )
 }
